f71abb4dc745dcd887999c976a3a62110027dfb6
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-03-11 16:35:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import modal from './modal';
 
 modal({
     modalSelector: '.popup',
-    modalSelectorDisplay: '(default: block)'
+    modalSelectorDisplay: '(default: block)',
     triggerSelector: '.popup-trigger',
     closeSelector: '.popup-close',
     timeSelector: '.popup',
